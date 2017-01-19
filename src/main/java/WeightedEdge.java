5e6b590eb31a9e224c1b87247cc3da665b2e0006
janra <jan.steffen.rausch@gmail.com>
2017-01-19 15:41:21 +0100

    jan fixing chooseNextNode

---
+++ src/main/java/WeightedEdge.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Benutzer01
+ */
+public class WeightedEdge extends Edge implements Comparable<WeightedEdge>{
+    Double weightedWay;
+    public WeightedEdge(Integer a, Integer b, Double weightedWay) {
+        super(a, b);
+        this.weightedWay=weightedWay;
+    }
+   
+
+   
+    @Override
+    public int compareTo(WeightedEdge o) {
+        return weightedWay.compareTo(o.weightedWay);
+    }
+    
+}
