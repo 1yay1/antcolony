c64b354b42f972e218b5d0b157de3bab08da13c7
Johannes Meintrup <jmeintrup@googlemail.com>
2017-01-22 18:16:58 +0100

    It works now.

--- src/main/java/AntColony.java
+++ src/main/java/AntColony.java
@@ -12,27 +12,24 @@ import java.util.concurrent.BlockingQueue;
  */
 public class AntColony {
     public static void main(String[] args) {
-        File f = new File("05-map-10x10-36border.txt");
+        File f = new File("06-map-100x100-50.txt");
         final Grid g = new Grid(f);
 
         double q0, alpha, beta, decayRate;
         double step = 0.1;
 
         q0 = 0.1;
-        alpha = 0.5;
-        beta = 0.9;
+        alpha = 5;
+        beta = 10;
         decayRate = 0.08;
 
         final BlockingQueue<List<Integer>> blockingQueue = new ArrayBlockingQueue(1000);
         Ant a = new SalesmanAnt(g, blockingQueue, 100, q0, alpha, beta, decayRate);
 
         System.out.print("Paras: q0: " + q0 + " alpha: " + alpha + " beta: " + beta + " t0: " + decayRate + "\n");
-        for (int i = 0; i < 2000; i++) {
+        for (int i = 0; i < 1000; i++) {
             a.buildPath();
-            if (a.calculateDistanceFromPath(a.getBestGlobalPath()) <= 36) {
-                System.out.println("\ti: " + i + " " + Arrays.toString(a.getBestGlobalPath().toArray()) + " D : " + a.calculateDistanceFromPath(a.getPath()));
-                break;
-            }
+            System.out.println("[" + i + "] " +" distance: " + a.calculateDistanceFromPath(a.getBestGlobalPath()));
         }
         System.out.println("\ti: " + 2000 + " " + Arrays.toString(a.getBestGlobalPath().toArray()) + " D : " + a.calculateDistanceFromPath(a.getPath()));
 
